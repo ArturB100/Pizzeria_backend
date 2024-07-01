1e18b4c84f5f8b7eba00cc3abcd06bfaf32a11e1
Artur Bochenek <artur119@vp.pl>
2024-07-01 15:19:13 +0200

    AddOrderRequest dto modified

--- Pizzeria/Dto/Request/AddOrderRequest.cs
+++ Pizzeria/Dto/Request/AddOrderRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace Pizzeria.Dto.Request;
 
-public record AddOrderRequest( int UserId, int AddressId, List<AddOrderDetailsRequest> Details );
+public record AddOrderRequest( int AddressId, List<AddOrderDetailsRequest> Details );
